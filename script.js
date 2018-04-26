8c07d36df507f2db95a14eee397c473ebdee0064
unknown <jtc7537@gmail.com>
2018-04-25 22:46:51 -0400

    updates

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 var bioDisplayed = false;
 
 $(".cat-nav-item").hover((function(){
-    $(this).css("color","white")
+    $(this).css("color","orange
+    ")
 }),function(){
-    $(this).css("color","black")
+    $(this).css("color","white")
 });
-
-$(".about-me-text").click(function(){
-    if(!bioDisplayed){ 
-        $(this).html("Hey! My name is Jonathan, I'm an SEO Analyst at a marketing agency in the triangle. If you'd like to learn more about me feel free to check out my portfolio or my Github, or send me a message on LinkedIn.<br><br>Cheers!")
-        bioDisplayed = true;
-        $("#overlay").css("visibility","hidden");
-    } else {
-        $(this).html("CLICK HERE")
-        bioDisplayed = false;
-        $("#overlay").css("visibility","visible");
-    }
-})
 
 $(".project").hover((function(){
     $(this).css("text-decoration","none").css("color","white")
